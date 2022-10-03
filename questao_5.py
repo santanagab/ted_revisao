5142dde43cadee682fba92ce61097caa53f80b5e
santanagab <santanna.gab@gmail.com>
2022-10-03 16:10:51 -0300

    modificacao

--- questao_5.py
+++ questao_5.py
@@ -8,8 +8,8 @@ primos = ''
 for i in range(1, numero + 1):
         cont = 0
         for x in range (1, i + 1):
-             if i % x == 0:
-                 cont += 1
+            if i % x == 0:
+                cont += 1
         if cont == 2:
             lista_primos.append(i)
             primos = str(lista_primos)[1:-1]
